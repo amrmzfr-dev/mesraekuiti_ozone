65b7a39f33d06d5ad60f8364f62089b53bc6493a
Izharudin Ismazi <izharudin02@gmail.com>
2025-09-19 15:26:41 +0800

    CSS
    CSS Updated

--- backend/ozontelemetry/settings.py
+++ backend/ozontelemetry/settings.py
@@ -126,8 +126,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+# DEBUG: enable for local dev
+DEBUG = True
+
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 
 # Media files
 MEDIA_URL = '/media/'
